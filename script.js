75b8b770b0b7491ce6e06cea7259e2dbd92407b6
Nulikontik <nurlanbekovulanbek@gmail.com>
2023-12-17 23:37:18 +0600

    some changes

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
-function login() {
-  // Добавьте вашу логику входа здесь
-  alert("Вы вошли!");
+async function login() {
+  const loginInput = document.getElementById("login").value;
+  const passwordInput = document.getElementById("password").value;
+
+  const response = await fetch("/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ login: loginInput, password: passwordInput }),
+  });
+
+  if (response.ok) {
+    alert("Вы успешно вошли!");
+  } else {
+    alert("Неверный логин или пароль");
+  }
 }
